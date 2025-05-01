51d830ca6ad9a3cadea091a7eb03c17e9a4c853b
Pi͢͢͢kสcђu <146358318+IR-O@users.noreply.github.com>
2025-05-01 16:20:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
-ADMIN_IDS = [int(id) for id in os.getenv('ADMIN_IDS', '').split(',') if id]
+TOKEN = os.getenv('TELEGRAM_BOT_TOKEN', "6251329523:AAFRYhYyoEYIkI-lIrpfkLeUla2g7lujv0I")
+ADMIN_IDS = [int(id) for id in os.getenv('ADMIN_IDS', '6045293810').split(',') if id]
 DATABASE_URL = os.getenv('DATABASE_URL', 'sqlite:///games.db')
